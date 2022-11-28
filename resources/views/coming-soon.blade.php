43a016953c989490bd201b9bbc834073c3dae67e
vagrant <vagrant@homestead>
2022-11-28 01:06:27 +0000

    Landing soon, fixed

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -17,32 +17,16 @@
 
   <body>
 
-
-    <!-- Header -->
-    <header class="header text-white h-fullscreen py-0" style="background-image: url(../assets/img/coming-soon.jpg); " data-overlay="9">
-      <div class="container text-center">
-
-        <div class="row h-100 align-items-center">
-          <div class="col-lg-8 mx-auto">
-
-            <h1 class="text-uppercase ls-3 fw-500">Landing Soon...</h1>
-            <br><br>
-            <p class="lead">
-              Medusa Money is transforming into Level8 Trading.
-            </p>
-            <br><br>
-            <img src="{{ url('assets/img/level8-white.png') }}" style="width: 50%"/>
-            <br><br>
-            <p>
-             <em>Trading Strategies | Technical Indicators | Trading Education </em>
-            </p>
-            <br><br><br><br><br><br><br><br><br><br>
-          </div>
-        </div>
-
-      </div>
-    </header><!-- /.header -->
-
+  <div class="vertical-container text-center text-white" style="background-image: url(../assets/img/coming-soon.jpg); background-size: cover;" data-overlay="9">
+    <div class="vertical-center">
+      <h1 class="text-uppercase ls-3 fw-500">Landing Soon...</h1>
+      <p class="lead">
+        Medusa Money is transforming into Level8 Trading.
+      </p>
+      <br>
+      <img src="{{ url('assets/img/level8-white.png') }}" id="cs-logo">
+    </div>
+  </div>
 
     <!-- Scripts -->
     <script src="{{ url('assets/js/page.min.js') }}"></script>
